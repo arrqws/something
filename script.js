c4fe93979eed0c4f47387e61bed66267eb5e3b08
arrqws <110641655+arrqws@users.noreply.github.com>
2022-08-04 19:15:37 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let list1=["thing", "mouse", "air", "water"]
+let list2=["something","rat", "ground", "fire"]
+let combinedlist=list1.concat(list2)
+console.log(combinedlist)
+list1.join("*")
+console.log(list1.join(" "))
+list1.reverse()
+console.log(list1)
+list1.reverse()
+list1.slice(1, 2+1)
+console.log(list1.slice(1, 2+1))
+list1.splice(2, 2)
+console.log(list1)
+list1.sort()
+console.log(list1.sort())
+let thiss="mrs.bartholoemew"
+thiss.slice(1, 2+1)
+console.log(thiss.slice(0, 3+1))
